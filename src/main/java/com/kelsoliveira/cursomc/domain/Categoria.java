37aea740352272d05ef841be8e2324b163c227a5
Kelson Oliveira <oliverkp@gmail.com>
2019-08-15 13:36:56 -0400

    Teste da classe de dominio [Categoria]

---
+++ src/main/java/com/kelsoliveira/cursomc/domain/Categoria.java
@@ -0,0 +1,80 @@
+//PACOTE QUE CONTEM AS CLASSES DE DOMINIO
+package com.kelsoliveira.cursomc.domain;
+
+//PACOTES DE IMPORTAÇÕES
+import java.io.Serializable;
+
+/* SERIALIZABLE IMPLEMENTADA PARA QUE OS OBJETOS SEJAM CONVERTIDOS EM UMA SEQUENCIA DE BYTES
+ * PARA QUE OS OBJETOS SEJAM GRAVADOS EM ARQUIVOS, TRAFEGAR EM REDE...
+ * SENDO UMA EXIGENCIA DO JAVA */
+
+//CLASSE DE DOMINIO DAS CATEGORIAS DE PRODUTOS
+public class Categoria implements Serializable {
+	private static final long serialVersionUID = 1L; //NUMERO DE VERSÃO DA CLASSE
+	
+	//ATRIBUTOS BÁSICOS DA CLASSE, POR PADRÃO SÃO PRIVADOS, NÃO ACESSIVEIS A OUTRAS CLASSES
+	private Integer idCat;
+	private String nome;
+	
+	//CONSTRUTOR DA CLASSE, VAZIO ONDE SE INSTANCIA OBJETOS SEM JOGAR NADA PARA OS ATRIBUTOS
+	public Categoria () {
+		
+	}
+
+	//CONSTRUTOR PARA POVOAR OS ATRIBUTOS
+	public Categoria(Integer idCat, String nome) {
+		super();
+		this.idCat = idCat;
+		this.nome = nome;
+	}
+
+	//METODOS DE ACESSO PARA OS ATRIBUTOS DA CLASSE	
+	public Integer getIdCat() {
+		return idCat;
+	}
+
+	public void setIdCat(Integer idCat) {
+		this.idCat = idCat;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	/* HASH CODE E EQUALS
+	 * EM JAVA PARA QUE DOIS OBJETOS SEJAM COMPARADOS POR SEU CONTEÚDO E NÃO PELO PONTEIRO DE MEMÓRIA
+	 * POR PADRÃO A COMPARAÇÃO É FEITA PELA O ID DA CLASSE, SE O ID FOR DIFERENTE OS OBJETOS TAMBÉM SERÃO */
+	
+	//GERA UM CODIGO NÚMERICO PARA CADA OBJETO
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idCat == null) ? 0 : idCat.hashCode());
+		return result;
+	}
+	//METODO DE COMPARAÇÃO ENTRE OS OBJETOS
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (idCat == null) {
+			if (other.idCat != null)
+				return false;
+		} else if (!idCat.equals(other.idCat))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
--- src/main/java/com/kelsoliveira/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/kelsoliveira/cursomc/resources/CategoriaResource.java
@@ -1,18 +1,31 @@
 //PACOTE QUE CONTEM AS CLASSES DE CONTROLE DE RECURSOS
 package com.kelsoliveira.cursomc.resources;
 
-//DEPENDENCIAS
+//LISTA DE IMPORTAÇÕES
+import java.util.ArrayList;
+import java.util.List;
+import com.kelsoliveira.cursomc.domain.Categoria;
 
+//DEPENDENCIAS
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-//CLASSE DE DOMINIO DO TIPO CONTROLADORA REST
+//CLASSE DO TIPO CONTROLADORA REST
 @RestController
 @RequestMapping(value = "/categorias") // EndPoint PARA ESSE RECURSO
 public class CategoriaResource {
 	@RequestMapping(method = RequestMethod.GET) //ASSOCIAÇÃO COM O VERBO HTTP - NO CASO OBTER DADOS - GET
-	public String listar() {
-		return "REST FUNCIONANDO";
+	public List<Categoria> listar() {
+		
+		Categoria cat1  = new Categoria (1,"Informática");
+		Categoria cat2 = new Categoria (2,"Escritório");
+		
+		/*List É UMA INTERFACE E NÃO PODE SER INSTANCIADO*/
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
